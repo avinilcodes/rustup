c957779b15ec83b00d6d34eb7b6741fd364cf93f
Avinil Bedarkar <avinilbedarkar@gmail.com>
2024-06-10 06:39:04 +0530

    day3 first half

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -28,4 +28,79 @@ fn main() {
     let o = 3.0;
     let p = i/o;
     println!("value of divison is {0:08.3}\n value of i is {1}\n value of p is {0}", p,i);
+
+    // Bitwise operator
+    let mut value = 0b0111_0101u8;
+    println!("value of value is {}", value);
+    println!("value of value is {:08b}", value);
+
+    // NOT
+    value = !value;
+    println!("value of value is {:08b}", value);
+
+    // AND
+    value = value & 0b1111_0111u8;
+    println!("value of value is {:08b}", value);
+    println!("value of value is {}", value & 0b0100_0000);
+
+    // OR
+    value = value | 0b0100_0000u8;
+    println!("value of value is {:08b}", value);
+
+    // XOR
+    value = value ^ 0b0101_0101u8;
+    println!("value of value is {:08b}", value);
+
+    // SHIFT
+    value = value << 4;
+    println!("value of value is {:08b}", value);
+
+    value = value >> 4;
+    println!("value of value is {:08b}", value);
+
+    // Boolean
+    let h = true;
+    let t = false;
+    println!("value of h is {}, value of t is {}", h, t);
+    println!("not of h {}", !h);
+    println!("h AND t is {}", h&t);
+    println!("h OR t is {}", h|t);
+    println!("h XOR t is {}", h^t);
+
+    let n = (h^t) | (h&t);
+    println!("value of n is {}", n);
+
+    // short-cutting OR and AND
+    let val = (h^t) || panic!();
+    // val = (h^t) && panic!(); // Panics here
+    println!("val {}", val);
+    
+    // Comparision operators
+    println!("value of h is {}, value of t is {}", h, t);
+    println!("h EQUALS t {}", h == t);
+    println!("h NOT EQUALS t is {}", h != t);
+    println!("h GREATER THAN t is {}", h>t);
+    println!("h LESS THAN t is {}", h<t);
+    println!("h GREATER THAN EQUAL TO t is {}", h>=t);
+    println!("h LESS THAN EQUAL TO t is {}", h<=t);
+
+    // char literal
+
+    let letter = 'a';
+    let number = '1';
+    let finger = '\u{261D}';
+    println!("{}\n{}\n{}\n", letter, number, finger);
+
+
+    // Challenge
+    let q = 13;
+    let w = 2.3;
+    let e: f32 = 120.0;
+    
+        /* YOUR CODE GOES HERE */
+    let average = (q as f64 + w + e as f64)/3.0;
+    
+    assert_eq!(average, 45.1);
+    println!("Test passed!");
+
 }
